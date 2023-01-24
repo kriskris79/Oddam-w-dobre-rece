f2b9a9af1e06dd093e91dec67887d6982a5198c9
Krzysztof Kakolewski <98468617+kriskris79@users.noreply.github.com>
2023-01-24 17:08:40 +0000

    Update firebase.js

--- oddam_w_dobre_rece/src/firebase.js
+++ oddam_w_dobre_rece/src/firebase.js
@@ -11,14 +11,7 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCmVBxxg86_J_4VrMBat1B9wnRK-cG-hDw",
-    authDomain: "oddam-w-dobre-rece-ec6b1.firebaseapp.com",
-    databaseURL: "https://oddam-w-dobre-rece-ec6b1-default-rtdb.firebaseio.com",
-    projectId: "oddam-w-dobre-rece-ec6b1",
-    storageBucket: "oddam-w-dobre-rece-ec6b1.appspot.com",
-    messagingSenderId: "393456299911",
-    appId: "1:393456299911:web:e252d0c2fad5b70360ed22",
-    measurementId: "G-4CQHVE8LXM"
+XXXXXX
 };
 
 // Initialize Firebase
