9726539aadc1799f8041569976d4e106578baf96
kriskris79 <krzysztofkakolewski@googlemail.com>
2023-01-26 23:25:21 +0000

    errors

--- oddam_w_dobre_rece/src/components/home/Register2.js
+++ oddam_w_dobre_rece/src/components/home/Register2.js
@@ -1,23 +1,46 @@
-import React, {useState, useEffect, useCallback} from 'react';
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-import app from "../../firebase";
+// import React, {useState, useEffect, useCallback} from 'react';
+// import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+// import app from "../../firebase";
 import decoration from "../../assets/Decoration.svg";
-import {Link} from "react-router-dom";
+import ReactDOM from "react-dom";
+import { useState } from "react";
+// import {Link} from "react-router-dom";
+
+//
+
+function App () {
+
+    const [registerEmail, setRegisterEmail] = useState ("");
+    const [registerPassword, setRegisterPassword] = useState ("");
+    const [loginEmail, setLoginEmail] = useState ("");
+    const [loginPassword, setLoginPassword] = useState ("");
+
+    const register = async () => {
+
+    }
+
+    const login = async  () => {
 
 
+    }
+
+    const logout = async () => {
+
+    }
+}
 const Register2 = ({ history}) => {
-    const handleRegister2 = useCallback(async event => {
-        event.preventDefault();
-        const { email, password } = event.target.elements;
-        try {
-            await app
-                .auth()
-                .createUserWithEmailAndPassword(email.value, password.value);
-            history.push("/");
-        } catch (error) {
-            alert(error);
-        }
-    }, [history]);
+//     const handleRegister2 = useCallback(async event => {
+//         event.preventDefault();
+//         const { email, password } = event.target.elements;
+//         try {
+//             await app
+//                 .auth()
+//                 .createUserWithEmailAndPassword(email.value, password.value);
+//             history.push("/");
+//         } catch (error) {
+//             alert(error);
+//         }
+//     }, [history]);
 // const Register2 = () => {
 //     const [email, setEmail] = useState("");
 //     const [password, setPassword] = useState("");
@@ -48,6 +71,7 @@ const Register2 = ({ history}) => {
         //guzik submit
         // https://firebase.google.com/docs/auth/web/password-auth#sign_in_a_user_with_an_email_address_and_password dodać automatycne logowanie po rejestracji
         <div>
+
             <h1>Załóż konto</h1>
             <img src={decoration}></img>
             <form onSubmit={Register2}>
@@ -57,18 +81,42 @@ const Register2 = ({ history}) => {
 
 
                 <label> Email
-                    <input type="email" name="email"   placeholder="email"/>
+                    <input type="email" name="email"   placeholder="email" onChenge={(event) => {setRegisterEmail(event.target.value);
+                    }} />
                 </label>
                 <label>
                     Password
 
-                    <input type="password" name="password" placeholder="Password"/>
+                    <input type="password" name="password" placeholder="Password" onChenge={(event) => {setRegisterPassword(event.target.value);
+                    }} />/>
                 </label>
 
                 <button type="submit" >Załóż konto</button>
 
+
+
+                < h1> Login</h1>
+                <label> Email- login
+                    <input type="email" name="email-login"   placeholder="email-login"  onChenge={(event) => {setLoginEmail(event.target.value);
+                    }} />/>
+                </label>
+                <label>
+                    Password- login
+
+                    <input type="password" name="password-login" placeholder="Password-login" onChenge={(event) => {setLoginPassword(event.target.value);
+                    }} />/>
+                </label>
+
+                <button type="submit" >Log in </button>
+
+
+
+                <h4> User log in </h4>
+                    <button>Sign Out </button>
+
             </form>
-        </div>
+         </div>
+
     );
 };
 
