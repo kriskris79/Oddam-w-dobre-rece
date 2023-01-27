396f354135f5230337eaa262137e2f03850029c9
kriskris79 <krzysztofkakolewski@googlemail.com>
2023-01-27 16:37:29 +0000

    no more errors

--- oddam_w_dobre_rece/src/components/home/Register2.js
+++ oddam_w_dobre_rece/src/components/home/Register2.js
@@ -1,49 +1,33 @@
-// import React, {useState, useEffect, useCallback} from 'react';
-// import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
-// import app from "../../firebase";
-// import decoration from "../../assets/Decoration.svg";
-// import ReactDOM from "react-dom";
 import { useState } from "react";
-// import {Link} from "react-router-dom";
 import {
     createUserWithEmailAndPassword,
     signInWithEmailAndPassword,
-onAuthStateChanged,
+    onAuthStateChanged,
     signOut,
-
 } from "firebase/auth";
 import { auth} from "../../firebase";
-// import register from "./Register";
-// import login from "./Login";
-// import {currentPositionY} from "react-scroll/modules/__tests__/utility";
-
-
 
 function Register2 () {
-
     const [registerEmail, setRegisterEmail] = useState ("");
     const [registerPassword, setRegisterPassword] = useState ("");
     const [loginEmail, setLoginEmail] = useState ("");
     const [loginPassword, setLoginPassword] = useState ("");
+    const [user,setUser] = useState({});
+    //
+    // onAuthStateChanged(auth, (currentUser) => {
+    //     setUser(currentUser);
+    // });
 
-    const [user , setUser] = useState({});
 
-    onAuthStateChanged(auth, (currentUser) => {
-setUser(currentUser);
-    });
-
-
-    const register = async () => {
-try {
-    const user = await createUserWithEmailAndPassword(
-        auth,
-        registerEmail,
-        registerPassword
-    );
-    console.log(user);
-} catch (error) {
-    console.log(error.message);
-}
+    const register = async (e) => {
+            e.preventDefault();
+            createUserWithEmailAndPassword(
+                auth,
+                registerEmail,
+                registerPassword
+            )
+            .then(use => console.log({user}))
+            .catch(error => console.log(error))
     };
 
     const login = async  () => {
@@ -61,106 +45,60 @@ try {
     };
 
     const logout = async () => {
-await signOut(auth);
+        await signOut(auth);
     };
-
-// const Register2 = ({ history}) => {
-//     const handleRegister2 = useCallback(async event => {
-//         event.preventDefault();
-//         const { email, password } = event.target.elements;
-//         try {
-//             await app
-//                 .auth()
-//                 .createUserWithEmailAndPassword(email.value, password.value);
-//             history.push("/");
-//         } catch (error) {
-//             alert(error);
-//         }
-//     }, [history]);
-// const Register2 = () => {
-//     const [email, setEmail] = useState("");
-//     const [password, setPassword] = useState("");
-//
-//     useEffect(() => {
-//     }, [])
-//
-//     const addUser = (e) => {
-//         e.preventDefault();
-//         const auth = getAuth(app);
-//         createUserWithEmailAndPassword(auth, email, password)
-//             .then((userCredential) => {
-//                 // Signed in
-//                 const user = userCredential.user;
-//                 // ...
-//             })
-//             .catch((error) => {
-//                 const errorCode = error.code;
-//                 const errorMessage = error.message;
-//                 // ..
-//             });
-//     }
-
+    console.log("test");
     return (
-
-        //dodać formularz z nasłuchem onSubmit na funkcję addUser
-        //dwa inputy email password
-        //guzik submit
-        // https://firebase.google.com/docs/auth/web/password-auth#sign_in_a_user_with_an_email_address_and_password dodać automatycne logowanie po rejestracji
-        <div>
-
+       <form onSubmit={register}>
             <h1>Załóż konto</h1>
-            {/*<img src={decoration}></img>*/}
-            {/*<form onSubmit={Register2}>*/}
                 <h1>Register</h1>
-
-
-
-
-                <label> Email
+                    <label> Email
                     <input
                         placeholder="email"
-                        onChenge={(event) => {
-                            setRegisterEmail(event.target.value);
+                        value={registerEmail}
+                        onChange={(event) => {
+                            setRegisterEmail(event.target.value)
                     }}
                     />
                 </label>
                 <label>
                     Password
-
                     <input
+                        type="password"
                         placeholder="Password"
-                        onChenge={(event) => {
-                            setRegisterPassword(event.target.value);
+                        value={registerPassword}
+                        onChange={(event) => {
+                            setRegisterPassword(event.target.value)
                     }}
-                    />
+                />
                 </label>
+                <button>Załóż konto</button>
 
-                <button onClick={register} >Załóż konto</button>
-
-
-
-                < h1> Login</h1>
-                <label> Email- login
-                    <input type="email" name="email-login"   placeholder="email-login"  onChenge={(event) => {setLoginEmail(event.target.value);
-                    }} />/>
-                </label>
-                <label>
-                    Password- login
-
-                    <input type="password" name="password-login" placeholder="Password-login" onChenge={(event) => {setLoginPassword(event.target.value);
-                    }} />/>
-                </label>
-
-                <button onClick={login}> Log in </button>
-
-                <h4> User log in </h4>
-            {user?.email}
-
-                    <button onClick={logout}> Sign Out </button>
-
-            {/*</form>*/}
-         </div>
-
+                {/*< h1> Login</h1>*/}
+                {/*<label> Email- login*/}
+                {/*    <input*/}
+                {/*        type="email"*/}
+                {/*        name="email-login"*/}
+                {/*        placeholder="email-login"*/}
+                {/*        value={loginEmail}*/}
+                {/*        onChange={(event) => {setLoginEmail(event.target.value)}}*/}
+                {/*    />*/}
+                {/*</label>*/}
+                {/*<label>*/}
+                {/*    Password- login*/}
+                {/*    <input*/}
+                {/*        type="password"*/}
+                {/*        name="password-login"*/}
+                {/*        placeholder="Password-login"*/}
+                {/*        value={loginPassword}*/}
+                {/*        onChange={(event) => {setLoginPassword(event.target.value)}}*/}
+                {/*    />*/}
+                {/*</label>*/}
+                {/*<button onClick={login}> Log in </button>*/}
+                {/*<h4> User log in </h4>*/}
+                {/*{user?.email}*/}
+                {/*<button onClick={logout}> Sign Out </button>*/}
+         </form>
     );
 };
 
---
+++ oddam_w_dobre_rece/src/components/home/Register3.js
@@ -0,0 +1,167 @@
+// import React, {useState, useEffect, useCallback} from 'react';
+// import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+// import app from "../../firebase";
+// import decoration from "../../assets/Decoration.svg";
+// import ReactDOM from "react-dom";
+import { useState } from "react";
+// import {Link} from "react-router-dom";
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+onAuthStateChanged,
+    signOut,
+
+} from "firebase/auth";
+import { auth} from "../../firebase";
+// import register from "./Register";
+// import login from "./Login";
+// import {currentPositionY} from "react-scroll/modules/__tests__/utility";
+
+
+
+function Register2 () {
+
+    const [registerEmail, setRegisterEmail] = useState ("");
+    const [registerPassword, setRegisterPassword] = useState ("");
+    const [loginEmail, setLoginEmail] = useState ("");
+    const [loginPassword, setLoginPassword] = useState ("");
+
+    const [user , setUser] = useState({});
+
+    onAuthStateChanged(auth, (currentUser) => {
+setUser(currentUser);
+    });
+
+
+    const register = async () => {
+try {
+    const user = await createUserWithEmailAndPassword(
+        auth,
+        registerEmail,
+        registerPassword
+    );
+    console.log(user);
+} catch (error) {
+    console.log(error.message);
+}
+    };
+
+    const login = async  () => {
+        try {
+            const user = await signInWithEmailAndPassword(
+                auth,
+                loginEmail,
+                loginPassword
+            );
+            console.log(user);
+        } catch (error) {
+            console.log(error.message);
+        }
+
+    };
+
+    const logout = async () => {
+await signOut(auth);
+    };
+
+// const Register2 = ({ history}) => {
+//     const handleRegister2 = useCallback(async event => {
+//         event.preventDefault();
+//         const { email, password } = event.target.elements;
+//         try {
+//             await app
+//                 .auth()
+//                 .createUserWithEmailAndPassword(email.value, password.value);
+//             history.push("/");
+//         } catch (error) {
+//             alert(error);
+//         }
+//     }, [history]);
+// const Register2 = () => {
+//     const [email, setEmail] = useState("");
+//     const [password, setPassword] = useState("");
+//
+//     useEffect(() => {
+//     }, [])
+//
+//     const addUser = (e) => {
+//         e.preventDefault();
+//         const auth = getAuth(app);
+//         createUserWithEmailAndPassword(auth, email, password)
+//             .then((userCredential) => {
+//                 // Signed in
+//                 const user = userCredential.user;
+//                 // ...
+//             })
+//             .catch((error) => {
+//                 const errorCode = error.code;
+//                 const errorMessage = error.message;
+//                 // ..
+//             });
+//     }
+
+    return (
+
+        //dodać formularz z nasłuchem onSubmit na funkcję addUser
+        //dwa inputy email password
+        //guzik submit
+        // https://firebase.google.com/docs/auth/web/password-auth#sign_in_a_user_with_an_email_address_and_password dodać automatycne logowanie po rejestracji
+        <div>
+
+            <h1>Załóż konto</h1>
+            {/*<img src={decoration}></img>*/}
+            {/*<form onSubmit={Register2}>*/}
+                <h1>Register</h1>
+
+
+
+
+                <label> Email
+                    <input
+                        placeholder="email"
+                        onChenge={(event) => {
+                            setRegisterEmail(event.target.value);
+                    }}
+                    />
+                </label>
+                <label>
+                    Password
+
+                    <input
+                        placeholder="Password"
+                        onChenge={(event) => {
+                            setRegisterPassword(event.target.value);
+                    }}
+                    />
+                </label>
+
+                <button onClick={register} >Załóż konto</button>
+
+
+
+                < h1> Login</h1>
+                <label> Email- login
+                    <input type="email" name="email-login"   placeholder="email-login"  onChenge={(event) => {setLoginEmail(event.target.value);
+                    }} />/>
+                </label>
+                <label>
+                    Password- login
+
+                    <input type="password" name="password-login" placeholder="Password-login" onChenge={(event) => {setLoginPassword(event.target.value);
+                    }} />/>
+                </label>
+
+                <button onClick={login}> Log in </button>
+
+                <h4> User log in </h4>
+            {user?.email}
+
+                    <button onClick={logout}> Sign Out </button>
+
+            {/*</form>*/}
+         </div>
+
+    );
+};
+
+export default Register2;
